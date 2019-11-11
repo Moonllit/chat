4c3ba36796fdafbc39c9e85ccdf2fc2848b85f68
dasha <fillatova.dasha@gmail.com>
2019-11-11 11:44:36 +0300

    Cache layer. Added: logout, get account, save account

--- app/src/main/java/com/mirallis/chat/cache/AccountCacheImpl.kt
+++ app/src/main/java/com/mirallis/chat/cache/AccountCacheImpl.kt
@@ -1,9 +1,10 @@
 package com.mirallis.chat.cache
 
 import com.mirallis.chat.data.account.AccountCache
+import com.mirallis.chat.domain.account.AccountEntity
 import com.mirallis.chat.domain.type.Either
 import com.mirallis.chat.domain.type.None
-import com.mirallis.chat.domain.type.exception.Failure
+import com.mirallis.chat.domain.type.Failure
 import javax.inject.Inject
 
 class AccountCacheImpl @Inject constructor(private val prefsManager: SharedPrefsManager) : AccountCache {
@@ -14,5 +15,17 @@ class AccountCacheImpl @Inject constructor(private val prefsManager: SharedPrefs
 
     override fun getToken(): Either<Failure, String> {
         return prefsManager.getToken()
+    }
+
+    override fun logout(): Either<Failure, None> {
+        return prefsManager.removeAccount()
+    }
+
+    override fun getCurrentAccount(): Either<Failure, AccountEntity> {
+        return prefsManager.getAccount()
+    }
+
+    override fun saveAccount(account: AccountEntity): Either<Failure, None> {
+        return prefsManager.saveAccount(account)
     }
 }
